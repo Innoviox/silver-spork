0cb7b240660d70bd404d591568a17fc91a424492
Simon Chervenak <sarras305@gmail.com>
2019-01-06 21:22:29 -0500

    linted

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -15,12 +15,11 @@ var Engine = Matter.Engine,
 
 // create engine
 var engine = Engine.create({
-            // enableSleeping: true
-        }),
+        // enableSleeping: true
+    }),
     world = engine.world;
 
 engine.world.gravity.y = 0;
-
 
 // create renderer
 var render = Render.create({
@@ -41,39 +40,57 @@ var runner = Runner.create();
 Runner.run(runner, engine);
 
 var is_off_screen = body => body.position.x > 800 ||
-                            body.position.x < 0 ||
-                            body.position.y > 600 ||
-                            body.position.y < 0;
+    body.position.x < 0 ||
+    body.position.y > 600 ||
+    body.position.y < 0;
 
 var NUMBER_OF_BODIES = 10;
-var bodyStyle = { fillStyle: '#0000FF' };
+var bodyStyle = {
+    fillStyle: '#0000FF'
+};
 var bodies = Array(NUMBER_OF_BODIES).fill().map(() => {
-  var size = Common.random(10, 20);
-  return Bodies.rectangle(Common.random(0, 800), Common.random(0, 600), size, size, {
-    force: {x: (Math.random() - 0.5) / 500, y: (Math.random() - 0.5) / 500 },
-    render: bodyStyle
-  } );
+    var size = Common.random(10, 20);
+    return Bodies.rectangle(Common.random(0, 800), Common.random(0, 600), size, size, {
+        force: {
+            x: (Math.random() - 0.5) / 500,
+            y: (Math.random() - 0.5) / 500
+        },
+        render: bodyStyle
+    });
 });
 
 World.add(world, bodies);
 
 Events.on(render, "afterRender", event =>
-  document.getElementById("offscreen").innerHTML = bodies.filter(is_off_screen)
-                                                         .map(i => i.id)
-                                                         .join(', '));
+    document.getElementById("offscreen").innerHTML = bodies.filter(is_off_screen)
+    .map(i => i.id)
+    .join(', '));
 
 function setPairsColor(color) {
-  return event => {
-    for (var pair of event.pairs) {
-      pair.bodyA.render.fillStyle = color;
-      pair.bodyB.render.fillStyle = color;
+    return event => {
+        for (var pair of event.pairs) {
+            pair.bodyA.render.fillStyle = color;
+            pair.bodyB.render.fillStyle = color;
+        }
     }
-  }
 }
 
-Events.on(engine, 'collisionStart',  setPairsColor('#00FF00'));
+Events.on(engine, 'collisionStart', setPairsColor('#00FF00'));
 Events.on(engine, 'collisionActive', setPairsColor('#FF0000'));
-Events.on(engine, 'collisionEnd',    setPairsColor('#0000FF'));
+Events.on(engine, 'collisionEnd', setPairsColor('#0000FF'));
+
+Events.on(engine, 'collisionStart', event => {
+    for (var pair of event.pairs) {
+        Body.setVelocity(pair.bodyA, {
+            x: 0,
+            y: 0
+        });
+        Body.setVelocity(pair.bodyB, {
+            x: 0,
+            y: 0
+        });
+    }
+});
 
 // add mouse control
 var mouse = Mouse.create(render.canvas),
